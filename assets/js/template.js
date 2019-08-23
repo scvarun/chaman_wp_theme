629c3c69d8e2509c282528b7c95066f06b6eaeb1
scvarun <varun9509@gmail.com>
2019-08-23 19:39:11 +0530

    Updated js

--- assets/js/template.js
+++ assets/js/template.js
@@ -197,6 +197,7 @@
         var changeClasses = function($input) {
           var $formGroup = $input.closest('.form-group');
           var $parent = $input.parent();
+          console.log($input[0]);
           if($input[0].checked) {
             $formGroup.addClass('input-checked');
             $parent.addClass('input-checked');
