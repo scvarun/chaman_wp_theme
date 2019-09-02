a93122472c292178c43cda70606ffe0450d5505f
scvarun <varun9509@gmail.com>
2019-09-02 12:47:17 +0530

    updated page template

--- page.php
+++ page.php
@@ -21,9 +21,38 @@ get_header();
     ?>
       
       <article id="page-<?php the_ID(); ?>" <?php post_class(); ?>>
-        <?php
-          the_content( "" );
-        ?>
+        <?php if (\Elementor\Plugin::$instance->db->is_built_with_elementor($post->ID)): ?>
+          <?php the_content( "" ); ?>
+        <?php else: ?>
+          <div class="container-fluid">
+            <div class="row">
+              <div class="col entry-thumbnail-side d-none d-sm-flex">
+                <figure class="entry-thumbnail">
+                  <?php
+                  the_post_thumbnail( 'unifato-archive' );
+                  ?>
+                </figure>
+              </div><!-- /.col -->
+
+              <main class="col-sm-6">
+                <figure class="entry-thumbnail d-block d-sm-none">
+                  <?php
+                  the_post_thumbnail( 'unifato-archive' );
+                  ?>
+                </figure>
+
+                <h1 class="entry-title h3" itemprop="headline">
+                  <?php the_title() ?>
+                </h1>
+
+                <div class="entry-content" itemprop="text">
+                  <?php the_content() ?>      
+                </div><!-- /.entry-content -->
+
+              </main><!-- /.col-sm-6 -->
+            </div><!-- /.row -->
+          </div><!-- /.container-fluid -->
+        <?php endif; ?>
       </article><!-- #post-<?php the_ID(); ?> -->
 
     <?php endwhile;
