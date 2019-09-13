bf055d0870446b5be98ac3a20e3a0e7bfe69425b
scvarun <varun9509@gmail.com>
2019-09-13 13:43:18 +0530

    updated js

--- assets/js/template.js
+++ assets/js/template.js
@@ -353,7 +353,7 @@
 
     enableMousewheelSmoothScroll: function() {
       var $window = $(window);
-      var throttled = lodash.throttle(customScroll, 5000, {trailing: true, leading: false});
+      var throttled = lodash.throttle(customScroll, 5000, {trailing: false, leading: true});
       document.addEventListener('mousewheel', customScroll, {passive: false});
       function customScroll(event) {
         var delta = 0
