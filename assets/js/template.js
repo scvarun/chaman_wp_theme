1c4ef0d4684e1cfdcbb88a56428caa7d24228d4e
scvarun <varun9509@gmail.com>
2019-09-13 14:49:20 +0530

    updated js

--- assets/js/template.js
+++ assets/js/template.js
@@ -375,7 +375,7 @@
             targets: 'html, body',
             scrollTop: finScroll,
             duration: 300,
-            easing: 'easeOutQuad',
+            easing: 'ease',
           });
         }
         if (event.preventDefault)
