911bb4d00cf8a2c2af050802afe52994df3dfadf
scvarun <varun9509@gmail.com>
2019-09-13 12:53:48 +0530

    updated js

--- assets/js/template.js
+++ assets/js/template.js
@@ -353,7 +353,7 @@
 
     enableMousewheelSmoothScroll: function() {
       var $window = $(window);
-      var throttled = lodash.debounce(customScroll, 500);
+      var throttled = lodash.debounce(customScroll, 1000);
       document.addEventListener('wheel', throttled, {passive: false});
       function customScroll(event) {
         var delta = 0
