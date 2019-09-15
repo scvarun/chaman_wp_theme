c63a49864c1061d10393b9880c0d1d314e8a3b7e
scvarun <varun9509@gmail.com>
2019-09-16 03:19:41 +0530

    updated js

--- assets/js/template.js
+++ assets/js/template.js
@@ -338,10 +338,10 @@
 
           if(window.innerWidth < 820) {
             $image[0].style.position = 'fixed';
-            $image[0].style.top = 'calc(' + $image.parent().offset().top + 'px + 1%)';
-            $image[0].style.bottom = 'calc(' + ($image.parent().offset().top + $image.parent().outerHeight()) + 'px + 1%)';
-            $image[0].style.minHeight = 'calc(' + $image.parent().height() + 'px + 0%)';
-            $image[0].style.maxHeight = 'calc(' + $image.parent().height() + 'px + 0%)';
+            $image[0].style.top = 'calc(' + $image.parent().offset().top + 'px + .5%)';
+            $image[0].style.bottom = 'calc(' + ($image.parent().offset().top + $image.parent().outerHeight()) + 'px + .5%)';
+            $image[0].style.minHeight = 'calc(' + $image.parent().height() + 'px + 1%)';
+            $image[0].style.maxHeight = 'calc(' + $image.parent().height() + 'px + 1%)';
           } else {
             $image[0].style.position = 'fixed';
             $image[0].style.top = 'calc(' + $image.parent().offset().top + 'px - 4%)';
